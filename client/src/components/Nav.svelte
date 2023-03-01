9b5ce5aa3b526ff7270b386353027b428fca6b9f
tknk0369 <76672482+tknk0369@users.noreply.github.com>
2023-03-01 15:32:12 +0900

    add scan page

--- client/src/components/Nav.svelte
+++ client/src/components/Nav.svelte
@@ -64,6 +64,7 @@
 		<ul class="menu p-4 w-80 bg-base-100 text-base-content">
 			<li><a href="/ranking">ランキング</a></li>
 			<li><a href="/reference">図鑑</a></li>
+			<li><a href="/scan">スキャン</a></li>
 			<li><a href="/settings">設定</a></li>
 		</ul>
 	</div>
---
+++ client/src/routes/scan/+page.svelte
@@ -0,0 +1,36 @@
+<script label="ts">
+	// @ts-nocheck
+
+	import Nav from '../../components/Nav.svelte';
+	let preview, fileinput;
+	const onFileSelected = (e) => {
+		let image = e.target.files[0];
+		let reader = new FileReader();
+		reader.readAsDataURL(image);
+		reader.onload = (e) => {
+			preview = e.target.result;
+		};
+	};
+</script>
+
+<Nav>
+	<h1 class="text-4xl text-center m-4">スキャン</h1>
+	<div class="flex flex-col">
+		<input
+			class="hidden"
+			type="file"
+			accept="image/*"
+			on:change={(e) => onFileSelected(e)}
+			bind:this={fileinput}
+		/>
+		<div class="m-4 flex flex-row justify-between">
+			<button class="btn" on:click={() => fileinput.click()}>画像を選択</button>
+			{#if preview != null}
+				<button class="btn">画像を送信</button>
+			{:else}
+				<button class="btn btn-disabled" on:click={() => {}}>画像を送信</button>
+			{/if}
+		</div>
+		<img class="m-4" src={preview} alt="" />
+	</div>
+</Nav>
