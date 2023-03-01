426f621765dfd7aa2c8bd8aa0c62ae2648f591cf
tknk0369 <76672482+tknk0369@users.noreply.github.com>
2023-03-01 14:38:26 +0900

    fix ranking

--- client/src/routes/ranking/+page.svelte
+++ client/src/routes/ranking/+page.svelte
@@ -1,14 +1,14 @@
 <script label="ts">
 	import Nav from '../../components/Nav.svelte';
-	const items = ['aaa', 'bbb', 'ccc'];
+	const items = ['aaadddd', 'bbb', 'ccc'];
 </script>
 
 <Nav>
 	<h1 class="text-4xl text-center m-4">ランキング</h1>
 	<ul class="flex flex-col items-center m-4">
 		{#each items as item, i}
-			<li class="card bg-base-100 shadow-xl m-4">
-				<span class="text-4xl m-4">{i + 1}位 {item}</span>
+			<li class="card bg-base-100 w-96 shadow-xl m-4">
+				<span class="text-4xl m-4 card-body">{i + 1}位 {item}</span>
 			</li>
 		{/each}
 	</ul>
