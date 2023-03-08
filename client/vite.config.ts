bc39fffda32a2283a90aa7271a6ec1f808bac55e
Maruo.S <raspi-maru2004@outlook.jp>
2023-03-08 15:04:31 +0900

    vite proxy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,15 @@ const config: UserConfig = {
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	server:{
+		proxy: {
+			"/api/v1/": {
+			target:"http://localhost:8000/",
+			changeOrigin: true,
+			rewrite: (path:string) => path.replace('/api/v1', '')
+			}
+		}
 	}
 };
 
